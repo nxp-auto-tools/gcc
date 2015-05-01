2c55a0a9a8c2a503287306ca267320ee627e1276
Yvan Roux <yvan.roux@linaro.org>
2015-05-01 14:01:33 +0200

    2015-05-01  Yvan Roux  <yvan.roux@linaro.org>
    	[Linaro Only] Add LINK_SPEC changes and configure-time option for
    	Cortex-A53 erratum 843419 workaround.
    
    	* configure.ac: Add --enable-fix-cortex-a53-843419 option.
    	* configure: Regenerate.
    	* config/aarch64/aarch64-elf-raw.h (CA53_ERR_843419_SPEC): Define.
    	(LINK_SPEC): Include CA53_ERR_843419_SPEC.
    	* config/aarch64/aarch64-linux.h (CA53_ERR_843419_SPEC): Define.
    	(LINK_SPEC): Include CA53_ERR_843419_SPEC.
    
    Change-Id: Iecd1a3e241fb90673b1a62fc056768dd14322a49

--- gcc/config/aarch64/aarch64-linux.h
+++ gcc/config/aarch64/aarch64-linux.h
@@ -43,8 +43,15 @@
   " %{mfix-cortex-a53-835769:--fix-cortex-a53-835769}"
 #endif
 
+#ifdef TARGET_FIX_ERR_A53_843419_DEFAULT
+#define CA53_ERR_843419_SPEC " --fix-cortex-a53-843419 "
+#else
+#define CA53_ERR_843419_SPEC ""
+#endif
+
 #define LINK_SPEC LINUX_TARGET_LINK_SPEC \
-                  CA53_ERR_835769_SPEC
+                  CA53_ERR_835769_SPEC \
+                  CA53_ERR_843419_SPEC
 
 #define GNU_USER_TARGET_MATHFILE_SPEC \
   "%{Ofast|ffast-math|funsafe-math-optimizations:crtfastmath.o%s}"
